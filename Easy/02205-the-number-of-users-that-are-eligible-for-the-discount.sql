8e05c4f3adc8e200089bc148f3ace8d4705c2d5b
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-02-05 20:48:10 -0600

    Create 02205-the-number-of-users-that-are-eligible-for-the-discount.sql

---
+++ Easy/02205-the-number-of-users-that-are-eligible-for-the-discount.sql
@@ -0,0 +1,14 @@
+-- counting total users tht satisfies both where conditions
+
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+--       Write your MySQL query statement below.
+      select count(distinct user_id) as user_cnt
+      from Purchases
+      where time_stamp between startDate and endDate
+      and amount >= minAmount
+  );
+END
+
+-- analytics quotient- 1
